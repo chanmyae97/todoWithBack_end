2f703dc1cc1c62e6d89bf3dada4204c688fbc57f
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-03-06 17:12:09 +0700

    backEnd Learning Token

---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import db from '../db.js';
+
+const router = express.Router();
+
+//Get  all todos for logged-in user
+
+router.get('/')
+
+// Create a new todo
+router.post('/', (req, res) =>{
+    
+})
+
+// Update a todo
+router.put('/:id', (req, res) =>{
+
+})
+
+// Delete a todo
+router.delete('/:id', (req, res) =>{
+
+})
+
+export default router;
