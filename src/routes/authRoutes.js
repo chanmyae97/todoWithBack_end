0147299e9dd99d242b6dd577a522cca4369d9b96
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-03-07 14:45:58 +0700

    auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -32,7 +32,30 @@ router.post('/register', (req, res) =>{
 })
 
 router.post('/login', (req, res) =>{
+    const {username, password} = req.body;
 
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?');
+        const user = getUser.get(username);
+
+        if (!user){
+            return res.status(404).send({message: 'User not found'})
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        
+        if(!passwordIsValid){
+            return res.status(401).send({message: 'Invalid password'})
+        }
+
+        // then we have a successful authentication
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'});
+        res.json({token});
+    } catch (err) {
+        console.log(err.message);
+        res.sendStatus(503);
+    }
 });
 
 export default router;
